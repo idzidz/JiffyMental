09eaa17db35a33e21085e9b4f4f75b180a145f37
idzidz <idz2612@gmail.com>
2022-06-13 20:04:38 -0400

    Crisis

--- jiffy-app/src/Crisis.js
+++ jiffy-app/src/Crisis.js
@@ -1,7 +1,49 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import "./index.scss"
+
 const Crisis = () => {
+
+    const handleCall = () => {
+        window.location.href = "tel:+18552423310";
+    }
+
+    const handleChat = () => {
+        window.location.href = "https://dcontario.org/";
+    }
+
+
+
+
+
+
     return(
         <div>
-            <h4 className="subText">todo: Crisis help page</h4>
+
+            <h2 className="headerText">We believe that you are currently in crisis</h2>
+
+            <h2 className="headerText">Please contact one of the resources below</h2>
+
+            <Container>
+
+                <Row>
+                    <Col> <h2 className="headerText">Hope for Wellness Help Line</h2> </Col>
+                </Row>
+
+                <Row style={{textAlign: "center", paddingBottom:"50px"}}>
+                    <Col> <button onClick={handleCall} style={{width:"300px"}}>Call now</button> </Col>
+                </Row>
+
+                <Row>
+                    <Col> <h2 className="headerText">Distress and Crisis Ontario</h2> </Col>
+                </Row>
+
+                <Row style={{textAlign: "center"}}>
+                    <Col> <button onClick={handleChat} style={{width:"300px"}}>Chat now</button> </Col>
+                </Row>
+
+
+
+            </Container>
 
         </div>
 
