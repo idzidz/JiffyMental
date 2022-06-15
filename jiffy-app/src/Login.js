c0e9378f8aa5dc2afdfc77acde76b04ba11fb4ed
idzidz <idz2612@gmail.com>
2022-06-15 17:08:05 -0400

    database connection established

--- jiffy-app/src/Login.js
+++ jiffy-app/src/Login.js
@@ -2,7 +2,6 @@ import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import {useNavigate} from "react-router-dom";
 import {Container, Row, Col} from 'react-bootstrap';
-import
 
 const Login = () => {
 
--- jiffy-app/src/database.js
+++ jiffy-app/src/database.js
@@ -1,16 +1,32 @@
 const {Client} = require('pg');
+require('dotenv').config();
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
-const client = new Client({
-    host: 'ec2-54-158-247-210.compute-1.amazonaws.com',
-    port: 5432,
-    user: 'cfpvsyppewaxza',
-    password: 'f138d7b391e513d3ce8a07c6ee3ee231b80ad5ff7ed0cdfec3cd45e05d165d6c',
-    database: 'dfva7bred9s76'
-})
+module.exports.getDatabase = async () => {
+    const client = new Client({
+        host: process.env.PG_HOST,
+        port: process.env.PG_PORT,
+        user: process.env.PG_USER,
+        password: process.env.PG_PASSWORD,
+        database: process.env.PG_DATABASE,
+        ssl: true,
+    });
+    await client.connect();
+    return client;
 
-client.connect();
+    // const result = await client.query('select $1::text as connected', ['true']);
+    // console.log(result.rows[0].connected);
 
-client.query('select * from doctor', (err, res) => {
-    console.log(err ? err.stack : res.rows)
-    client.end()
-})
+    // const result = await client.query('select * from users where username = $1', ['izelj']);
+    // console.log(result.rows[0]);
+
+    // This is how we pull the specific values that we need from the database.
+    // Will return undefined for logging of row[0], or an empty array in the mapper if no matches are found.
+
+    // const mapper = result.rows.map(row => Object.values(row));
+    // console.log(mapper);
+    // console.log(mapper[0][1]);
+    //
+    // await client.end();
+
+};
