c7aa15f3bd0764e2916d520c6251335306e35470
idzidz <idz2612@gmail.com>
2022-07-11 21:15:54 -0400

    Emailer currently not working due to change in Google policy: As of May 30 google no longer supports the use of third-party apps using only username and password

--- jiffy-app/src/Emailer/index.js
+++ jiffy-app/src/Emailer/index.js
@@ -10,6 +10,54 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: credentials.emailer.user,
+        pass: credentials.emailer.pass
+    }
+});
+
+
+
+app.post('/emailer', (req, res, next) => {
+    const email = req.body.email;
+    const message = req.body.message;
+    const subject = req.body.subject;
+    const name = req.body.name;
+    const company = req.body.company;
+
+    const mailOptions = {
+        from: name,
+        to: email,
+        subject: subject,
+        html: '${name} from ${company} <noreply@$' +
+            '{name}.com <br /> ${message}'
+    }
+
+    transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+            console.log(err);
+            res.sendStatus(500);
+        } else {
+            console.log(info);
+            res.sendStatus(200);
+        }
+    });
+});
+
+transporter.verify(function(error, success) {
+    if (error) {
+        console.log(credentials.emailer.user);
+        console.log(credentials.emailer.pass);
+        console.log(error);
+    } else {
+        console.log('Server is ready to take our messages');
+    }
+});
+
 const port = 3030;
 app.listen(port, () => {
     console.log("emailer has started on port 3030");
